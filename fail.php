5c6a8cf703d601a935f2677a16efc51b42cf223c
Ilkin Guluzada <iguluzada2020@ada.edu.az>
2017-06-03 23:06:16 +0400

    Add files via upload

---
+++ fail.php
@@ -0,0 +1,9 @@
+
+ 
+<link rel="stylesheet" href="/css/bootstrap.min.css">
+ 
+<script src="/js/bootstrap.min.js"></script>
+<div class="alert alert-success">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+<strong>Ödəniş uğursuz oldu!</strong>
+</div>
